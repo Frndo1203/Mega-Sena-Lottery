31e5c6a13c21190c2b2ba4b3de68df520b27aa2d
FERNANDO OLIVEIRA <oliveirafer06@gmail.com>
2023-01-02 19:36:15 -0300

    Implemented logic to generate random numbers and return to user, based on the entered value.

--- app/src/main/java/tech/fernandooliveira/megasena/MainActivity.kt
+++ app/src/main/java/tech/fernandooliveira/megasena/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +30,48 @@ class MainActivity : AppCompatActivity() {
 
         // option3: declaring directly the code block used by the onClick method
         btnGenerate.setOnClickListener {
-            Log.i("Test", "button clicked")
+
+            val text = editText.text.toString()
+            numberGenerator(text, txtResult)
+
+        }
+    }
+
+    private fun numberGenerator(text: String, txtResult: TextView) {
+        // validate if null or empty
+        if (text.isNotEmpty()) {
+            // convert to int
+            val intText: Int = text.toInt()
+
+            // validate if number is between 6 and 15
+            if (intText in 6..15) {
+                val numbers = mutableSetOf<Int>()
+                val random = Random()
+
+                while (true) {
+                    val number = random.nextInt(60)
+                    numbers.add(number + 1)
+
+                    Log.i("numbers generated", "Generated number: $number")
+
+                    if (numbers.size == intText) {
+                        break
+                    }
+                }
+
+                txtResult.text = numbers.joinToString(" - ")
+
+            } else {
+                Toast.makeText(this, "Enter a number between 6 and 15", Toast.LENGTH_SHORT).show()
+            }
+
+        } else {
+            Toast.makeText(this, "Please, enter a number", Toast.LENGTH_SHORT).show()
         }
 
 
-        }
+
+    }
 
     // option1: xml
     //    fun buttonClicked(view: View) {
